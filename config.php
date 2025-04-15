ac7782cea61bd2d72a0c8fc1ab02bcd8cacb75e4
zhuchenyu2008 <132262479+zhuchenyu2008@users.noreply.github.com>
2025-04-15 20:26:49 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'api_base' => 'https://api.gpt.ge', //末尾不要加“/”
+    'api_token' => 'your_token_here', //API－key
+    'usage_path' => '/v1/dashboard/billing/usage', //用量查询接口
+    'subscription_path' => '/v1/dashboard/billing/subscription', //限额查询接口
+];
